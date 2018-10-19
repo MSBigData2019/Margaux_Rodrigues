e99ad5866beeb090312e91baf7145b70d9bb8c0d
margauxRodrigues <margaux.rodrigues@telecom-paristech.fr>
2018-10-19 12:22:50 +0200

    Exercise done without multi thread (long to run ...)

--- Lesson3/exo_dom_lesson3.py
+++ Lesson3/exo_dom_lesson3.py
@@ -9,6 +9,8 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import json
+from queue import Queue
+from threading import Thread
 
 def get_main_contrib_crawl(url):
     
@@ -37,21 +39,61 @@ def get_main_contrib_crawl(url):
     
     d = {'Username': usernames, 'Name' : names}
     df = pd.DataFrame(data = d, index = rankings)
-    print(df)
+    return df
         
+def get_total_stars_for_user(user, tk, nbre_repos):
+    pages_to_visit = (nbre_repos // 100) + 1
+    stars = 0
+    for i in range(pages_to_visit):
+        url = 'https://api.github.com/users/' + user + '/repos?page='+ str(i) + 'per_page=100'
+        head = {'Authorization': 'token {}'.format(tk[0].strip())}
+        get_repo = requests.get(url, headers=head)
+        repo_dict = json.loads(get_repo.content) #return un dico de repos
+        for repo in repo_dict:
+            stars += repo['stargazers_count']
+    return stars
+
+def get_count_of_repos_for_user(user, tk):
+    url = 'https://api.github.com/users/' + user
+    head = {'Authorization': 'token {}'.format(tk[0].strip())}
+    get_repo = requests.get(url, headers=head)
+    user_dict = json.loads(get_repo.content) #return un dico de repos
+    repos_count = user_dict['public_repos']
+    return repos_count
+    
+def means_stars_per_user(user, tk):
+    repos_count = get_count_of_repos_for_user(user, tk)
+    stars = get_total_stars_for_user(user, tk, repos_count)
+    if repos_count != 0:
+        return [stars, repos_count, stars / repos_count]
+    else: return [stars, repos_count, 0] 
 
 def main():
     # Crawling
     url = 'https://gist.github.com/paulmillr/2657075'
+    tk_file = '../tk.txt'
+    with open (tk_file) as f:
+        tk = f.readlines()
     # get_main_contrib_crawl(url)
     # using API
     # objectif récupérer pour les top contributeurs le nombre moyen de stars de leurs repos
     # puis les classer 
-    url = 'https://api.github.com/users/StephanePEILLET/repos'
-    head = {'Authorization': 'token {}'.format('token')}
-    get_repo = requests.get(url, headers=head)
-    truc = json.loads(get_repo.content)
-    print(truc)
+
+    user_list = get_main_contrib_crawl(url) # returns a pd DF with Username
     #stragazers_count
+    stars_list = []
+    count_list = []
+    mean_list = []
+    for user in user_list.Username:
+        stars_means = means_stars_per_user(user, tk)
+        stars_list.append(stars_means[0])
+        count_list.append(stars_means[1])
+        mean_list.append(stars_means[2])
+    
+    user_list['Total_stars'] = stars_list
+    user_list['Number_repos'] = count_list
+    user_list['Mean_stars'] = mean_list
+    print(user_list.sort_values(by = 'Mean_stars', ascending = False))
+    
 if __name__ == '__main__':
     main()
