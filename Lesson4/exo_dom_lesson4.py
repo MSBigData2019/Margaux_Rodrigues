4e360124583ffd90f76dd67725c4a9d51ae19b92
margauxRodrigues <margaux.rodrigues@telecom-paristech.fr>
2018-10-27 12:31:12 +0200

    add one file to ignore

--- Lesson4/exo_dom_lesson4.py
+++ Lesson4/exo_dom_lesson4.py
@@ -19,33 +19,66 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import pandas as pd
-import json
 import re
 
-url = 'https://www.lacentrale.fr/listing?makesModelsCommercialNames=RENAULT%3AZOE&regions=FR-IDF%2CFR-PAC%2CFR-NAQ'
-req = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
-if req != 200:
-    print('Echec de la requête')
-    exit
-soup = BeautifulSoup(req.content)
+def soup_request(url):
+    req = requests.get(url, headers = {'User-Agent': 'Mozilla/5.0'})
+    if req.status_code != 200:
+        print('Echec de la requête')
+        exit
+    soup = BeautifulSoup(req.text, 'html.parser')
+    return soup
 
+# get price
+def get_car_price_from_soup(soup):
+    a = soup.find('div', {'class':'gpfzj'})
+    price = str(a.find_next('strong').text)
+    price_regex = re.compile(r"\d+\s+\d+")
+    price_find = re.search(price_regex, price).group(0)
+    price_find = re.sub(r"\s+", "", price_find)
+    return int(price_find)
+
+# Autres infos
+def get_year_from_soup(soup):
+    infos_generales = soup_annonce.find('ul', {'class' : 'infoGeneraleTxt column2'})
+    return int(infos_generales.findNext('span').text)
+
+def get_mileage_from_soup(soup):
+    infos_generales = soup_annonce.find('ul', {'class' : 'infoGeneraleTxt column2'})
+    kilometrage = infos_generales.findNext('span').findNext('span').text
+    kilometrage = re.sub(r"\s+|[a-z]", "", kilometrage)
+    return int(kilometrage)
+
+def add_ad_to_data(soup, ad_number, data):
+    data['ad_number'].append(ad_number)
+    data['price, km, year'].append([get_car_price_from_soup(soup), get_mileage_from_soup(soup), get_year_from_soup(soup)])
+# -------------------------------------------
+    
+brand  = 'RENAULT'
+model  = 'ZOE'
+region = 'IDF%2CFR-PAC%2CFR-NAQ'
+url = 'https://www.lacentrale.fr/listing?makesModelsCommercialNames=' + brand + '%3A' + model + '&regions=FR-' + region
+html = requests.get(url)
+soup = BeautifulSoup(html.content, features="lxml")
 # Get number of responses for request
 number_of_cars = int(soup.find('span', {'class':'numAnn'}).text)
 
+# Create dataset
+cars_renault_zoe = {'ad_number' : [],
+                    'price, km, year' : []
+                    }
+links=[]
+link_list = soup.findAll('a', class_='linkAd ann')
+for link in link_list:
+    links.append(link['href'])
 
 # ouvrir une annonce :
-url_annonce = 'https://www.lacentrale.fr/auto-occasion-annonce-69103378129.html'
-req_annonce = requests.get(url_annonce, headers = {'User-Agent': 'Mozilla/5.0'})
-if req_annonce != 200:
-    print(req_annonce)
-    print('Echec de la requête')
-soup_annonce = BeautifulSoup(req_annonce.content)
-a = soup_annonce.find('div', {'class':'gpfzj'})
-price = str(a.find_next('strong').text)
-price_regex = re.compile(r"\d\s+\d+")
-price_find = int(re.search(price_regex, price).group(0))
+prefix = 'https://www.lacentrale.fr'
+for i in range(len(links)):
+    url_annonce = prefix + links[i]
+    soup_annonce = soup_request(url_annonce)
+    add_ad_to_data(soup_annonce, links[i], cars_renault_zoe)
 
-# Autres infos
-infos_generales = soup_annonce.find('ul', {'class' : 'infoGeneraleTxt column2'})
-annee = infos_generales.findNext('span')
-kilometrage = annee.findNext('span')
+# create dataframe with results
+df = pd.DataFrame(cars_renault_zoe)
+df2 = pd.DataFrame(df['price, km, year'].values.tolist(), columns=['Price', 'Km', 'Year'])
